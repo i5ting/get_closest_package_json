a35b356631884833c37282f531e43e4cdb342cb4
i5ting <i5ting@126.com>
2016-07-25 09:35:30 +0800

    lazycommmit add file, please ignore

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # get_closest_package_json
+
+
+## Install
+
+```
+$ [sudo] npm i -S get_closest_package_json
+```
+
+## Usages
+
+```
+var config = require('get_closest_package_json')
+```
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var fs = require('fs')  
+var debug = require('debug')('get_closest_package_json')
+
+function get_closest_package_json () {
+  var config
   
-module.exports = function get_closest_package_json(){
-  module.paths.forEach(function(i){
+  module.paths.forEach(function (i) {
     var file = i.replace('node_modules', 'package.json')
     
     if (fs.existsSync(file) === true) {
       try{
-        console.log('exist = ' + file)
-        return require(file)
+        debug('exist file = ' + file)
+        // debug(require(file))
+        config =  require(file)
       }catch (e){
         console.error('get_closest_package_json' + e)
       }
+    } else {
+      debug('not exist file = ' + file)
     }
   })
+  
+  return config
 }
+
+module.exports = get_closest_package_json()
