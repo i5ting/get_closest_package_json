87b4c46d0d8844a8672a674567905e677eb9a5e4
i5ting <i5ting@126.com>
2016-07-25 09:48:31 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ var debug = require('debug')('get_closest_package_json')
 
 function get_closest_package_json () {
   var config
+  var isNext = true
   
   module.paths.forEach(function (i) {
     var file = i.replace('node_modules', 'package.json')
     
-    if (fs.existsSync(file) === true) {
+    if (isNext && fs.existsSync(file) === true) {
       try{
+        // break with flag
+        isNext = false
+        
+        // log
         debug('exist file = ' + file)
-        // debug(require(file))
+        
+        // get package.json content
         config =  require(file)
       }catch (e){
         console.error('get_closest_package_json' + e)
